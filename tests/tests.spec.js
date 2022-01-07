88edd05aca0062aebb16d349a2798875a39d2d64
lauramanzini <232566@studenti.unimore.it>
2022-01-07 12:16:08 +0000

    Testing beginned

---
+++ tests/tests.spec.js
@@ -0,0 +1,30 @@
+var should = require('chai').should(),
+    repo = require('../repo-rename'),
+    getRepoId = repo.getRepoId,
+    renameRepo = repo.renameRepo;
+
+const owner = 'ULL-ESIT-DMSI-1920',
+const name = 'prueba-laura'
+const id = '',
+const newName = '';
+
+
+describe('Get repository information', function(){
+    it('\t gets  repository owner', function(){
+        getRepoId('ULL-ESIT-DMSI-1920', null).should.equal(getRepoId(owner, null));
+    });
+
+    it('\t gets repository name', function(){
+        getRepoId(null, 'prueba-laura').should.equal(getRepoId(null,));
+    });
+});
+
+describe('Change repository name', function(){
+    it('\t gets repository id', function(){
+        getRepoId('ULL-ESIT-DMSI-1920', null).should.equal(getRepoId(owner, null));
+    });
+
+    it('\t gets repository new name', function(){
+        getRepoId(null, 'prueba-laura').should.equal(getRepoId(null,));
+    });
+});
