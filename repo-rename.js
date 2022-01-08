63489190d9fcf2035d29a986225b906076ffd235
lauramanzini <232566@studenti.unimore.it>
2022-01-08 15:15:00 +0000

    Added tests and changed node.js.yml

--- repo-rename.js
+++ repo-rename.js
@@ -39,7 +39,7 @@ function renameRepo (id, newName) {
         updateRepository(input: { name: "${newName}", repositoryId: "${id}" }) {
         repository{
             name
-        }
+            }
         }
     }
     `;
--- tests/tests.js
+++ tests/tests.js
@@ -1,13 +1,8 @@
 var should = require('chai').should(),
+    assert = require('assert'),
     repo = require('../repo-rename'),
     getRepoId = repo.getRepoId,
     renameRepo = repo.renameRepo;
-
-/**
-    const owner = 'ULL-ESIT-DMSI-1920',
-    const name = 'prueba-laura'
- */
-
 
 const id = 'R_kgDOGbkokQ';
 const newName = 'prueba-laura-funciona';
@@ -17,14 +12,27 @@ describe('Get repository information', function(){
         getRepoId.should.be.lengthOf(2);
     });
 
-    it('\t gets  repository id', function(){
+    it('\t gets repository id through should function', function(){
         getRepoId('ULL-ESIT-DMSI-1920', 'prueba-laura').should.equal(id);
+    });
+
+    it('\t gets repository Id through assert function', function(){
+        assert.equal(getRepoId('ULL-ESIT-DMSI-1920','prueba-laura'), id)
+    });
+
+    it('\t repository id is not equal to another repository id', function(){
+        let wrongId = 'R_kgDOGbeYPw';
+        assert.notEqual(getRepoId('ULL-ESIT-DMSI-1920','prueba-laura'), wrongId, 'Not the right id')
     });
 });
 
 
   describe('Change repository name', function(){
-    it('\t gets repository id', function(){
+    it('\t gets repository new name', function(){
         renameRepo(id, 'prueba-laura-funciona').should.equal(newName);
+    });
+    it('\t verify', function(){
+        let wrongName = 'prueba-anna-funciona'
+        assert.notEqual(renameRepo(id, 'prueba-laura-funciona'), wrongName )
     });
 });
